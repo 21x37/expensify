399550d5a0575f19fcf0f348f70184393e35915f
21x37 <tylermok@outlook.com>
2019-02-02 17:30:18 -0800

    Added redux store

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import getVisibleExpenses from './selectors/expenses';
+import { setTextFilter } from './actions/filters';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-import AppRouter from './routers/AppRouter';
+
 
 
 
