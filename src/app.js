4f8b2044b4de5850f8a03a0fae2f684e8ff56fdb
21x37 <tylermok@outlook.com>
2019-02-03 10:22:57 -0800

    started rendering redux state with react component

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
@@ -9,6 +10,26 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 
+const store = configureStore();
+
+store.dispatch(addExpense({ description: 'Water bill'}));
+store.dispatch(addExpense({ description: 'Gas bill'}));
 
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+store.dispatch(setTextFilter('water'));
+
+setTimeout(() => {
+    store.dispatch(setTextFilter('bill'));
+}, 3000)
+
+const state = store.getState();
+const visibleExpense = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpense);
+
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+)
+
+ReactDOM.render(jsx, document.getElementById('app'));
