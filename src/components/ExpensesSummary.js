960c55e315cf38ab015c53168bfc8f81a6118bd8
21x37 <tylermok@outlook.com>
2019-02-10 13:56:28 -0800

    Added login page and header styles

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import selectExpensesTotal from '../selectors/expenses-total';
 import selectExpenses from '../selectors/expenses';
 import numeral from 'numeral';
@@ -11,8 +12,13 @@ export const ExpensesSummary = ({ expensesCount, expensesTotal }) => {
     const formattedEpxensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
 
     return (
-        <div>
-            <h1>Viewing {expensesCount} {expenseWord} totalling {formattedEpxensesTotal}</h1>
+        <div className="page-header">
+            <div className='content-container'>
+                <h1 className='page-header__title'>Viewing <span>{expensesCount}</span> {expenseWord} totalling <span>{formattedEpxensesTotal}</span></h1>
+                <div className='page-header__actions'>
+                    <Link className='button' to='/create'>Add Expense</Link>
+                </div>
+            </div>
         </div>
     )
 };
